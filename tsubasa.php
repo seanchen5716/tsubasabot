768af95ebbff67e1bdbfdea6f82d7b34d14d5c93
SeanChen_Work <SeanChen_work@SeanChen-Work.local>
2016-04-10 08:27:03 +0900

    sdfsdgfs

--- tsubasa.php
+++ tsubasa.php
@@ -139,7 +139,7 @@ function api_post_request($path, $post) {
         "X-Line-ChannelSecret: {$GLOBALS['channel_secret']}",
         "X-Line-Trusted-User-With-ACL: {$GLOBALS['mid']}"
     );
-    curl_setopt($curl , CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl , CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\bin\curl-ca-bundle.crt');
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -162,7 +162,7 @@ function api_get_user_profile_request($mid) {
         "X-Line-ChannelSecret: {$GLOBALS['channel_secret']}",
         "X-Line-Trusted-User-With-ACL: {$GLOBALS['mid']}"
     );
-    curl_setopt($curl , CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl , CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\bin\curl-ca-bundle.crt');
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -178,7 +178,7 @@ function api_get_message_content_request($message_id) {
         "X-Line-ChannelSecret: {$GLOBALS['channel_secret']}",
         "X-Line-Trusted-User-With-ACL: {$GLOBALS['mid']}"
     );
-    curl_setopt($curl , CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl , CURLOPT_CAINFO, getenv('ProgramFiles(x86)') . '\Git\bin\curl-ca-bundle.crt');
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
