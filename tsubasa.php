9c5fd2b6bd84370a4bca5276d6a01b64e8e989b1
SeanChen_Work <SeanChen_work@SeanChen-Work.local>
2016-04-10 19:59:58 +0900

    ふぇ

--- tsubasa.php
+++ tsubasa.php
@@ -26,7 +26,7 @@ $message_id = $content->id;
 $content_type = $content->contentType;
 
 // 天気情報を取得
-$json_data = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&APPID=3239915d4dc6061371ef340d59722e6f');
+$json_data = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&APPID=3239915d4dc6061371ef340d59722e6f');
 $data = json_decode($json_data);
 
 $text .= '東京の天気は「' . $data->weather[0]->description  . '」らしいよ。';
