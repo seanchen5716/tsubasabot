c7839d15b0cf36d0610b341f73d7b0fed0f7a50d
SeanChen_Work <SeanChen_work@SeanChen-Work.local>
2016-04-12 23:45:26 +0900

    fixed

--- tsubasa.php
+++ tsubasa.php
@@ -157,7 +157,10 @@ curl_close($curl);
 // 株価抽出
 if (preg_match("/<td\sclass\=\"stoksPrice\"\>(\d+)\<\/td\>/", $body, $m)) {
   $price = $m[0];
-  $content = $stock_m;
+  $content = = <<< EOM
+          "contentType":1,
+          "text":"{$price}円"
+EOM;
 }
 }
 }else if($text == "天気"){
